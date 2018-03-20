9dcd8f42b80b9e6d764e891d163fde871762ba77
davihdsantos <davihenriqueds@gmail.com>
2018-03-20 11:15:07 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nboat
+# sailboat-control
+Sailboat develop, control and navigation. Implemented in C++ (Arduino) parts of the code are writen on portuguese. we are working to solve that.
+
+This repository contain the ongoing work on the development of a sailboat robot. The curent version of the sailboat uses arduino UNO and arduino MEGA and we provide the source code for control in C++.
+
+Vamos continuamente adicionar os trabalhos já realizados, buscando manter os códigos deste repositórios pronto para uso. the trying to keep this repository up to date.
+
+<p align="center">
+  <img src="./images/sailboat_eletronics.pdf" width="400" alt="sailboat hardware system"/>
+</p>
---
+++ arduino_mega_control/gps.ino
@@ -0,0 +1,58 @@
+void atualizar_posicao(struct location local){  
+  unsigned long start = millis();
+  unsigned long ms = 500;
+  do 
+  {
+    while(Serial3.available())
+    {
+      int c = Serial3.read();
+      if(gps.encode(c))
+      {
+        gps.f_get_position(&local.latitude, &local.longitude);
+        direcao = gps.f_course();
+        velocidade = gps.f_speed_kmph();
+      }
+    }
+  } while (millis() - start < ms);
+  //distanciaAoDestino = (int)TinyGPS::distance_between(latitudeBarco, longitudeBarco, latitudeDestino, longitudeDestino);  
+}
+
+int calcular_distancia(struct location p1, struct location p2){
+  return (int)TinyGPS::distance_between(p1.latitude, p1.longitude, p2.latitude, p2.longitude);
+}
+
+float calcular_orientacao(struct location p1, struct location p2){
+  float sp_aux = TinyGPS::course_to(p1.latitude, p1.longitude, p2.latitude, p2.longitude);
+  /*Serial.print("p1->lat: ");
+  Serial.print(p1.latitude, 6);
+  Serial.print(" ");
+  Serial.print("p1->lon: ");
+  Serial.print(p1.longitude, 6);
+  Serial.print(" ");
+  Serial.print("p2->lat: ");
+  Serial.print(p2.latitude, 6);
+  Serial.print(" ");
+  Serial.print("p2->lon: ");
+  Serial.print(p2.longitude, 6);
+  Serial.print(" ");
+  Serial.print("Course to -> ");
+  Serial.println(sp_aux);*/
+  //float sp_aux = TinyGPS::course_to(-5.842960, -35.197244, -5.842988, -35.196384);
+  if(sp_aux > 180){
+    sp_aux = sp_aux-360;
+  }
+  if(sp_aux < -180){
+    sp_aux = sp_aux+360;
+  }
+  return sp_aux;
+}
+
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (Serial3.available())
+      gps.encode(Serial3.read());
+  } while (millis() - start < ms);
+}
