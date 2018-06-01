15cf4098432155fa244c55e5080e880f98112bfe
davihdsantos <davihenriqueds@gmail.com>
2018-06-01 16:48:44 -0300

    added plots script (python)

---
+++ plots/plotExample.py
@@ -0,0 +1,35 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('data/16_54_13.CSV', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    reader = list(reader)
+    header = reader[0]
+    reader.pop(0) 
+
+    reader = [list(x) for x in zip(*reader)]
+
+#    print len(reader[2])
+    
+    TimeStamp = [float(i) for i in reader[1]]
+    Latitude = [float(i) for i in reader[2]]
+    Longitude = [float(i) for i in reader[3]]
+#    WindDirection = [float(i) for i in reader[4]]
+#    WindSpeed = [float(i) for i in reader[5]]
+    RudderAngle = [float(i) for i in reader[6]]
+    SailAngle = [float(i) for i in reader[7]]
+    GPSCourse = [float(i) for i in reader[8]]
+    GPSSpeed = [float(i) for i in reader[9]]
+    Yaw = [float(i) for i in reader[10]]
+    Pitch = [float(i) for i in reader[11]]
+    Roll = [float(i) for i in reader[12]]
+    Magnetometer = [float(i) for i in reader[13]]
+
+#    plt.plot(Latitude,Longitude)
+#    plt.plot(TimeStamp, RudderAngle)
+#    plt.plot(TimeStamp, SailAngle)
+#    plt.plot(TimeStamp, GPSCourse)
+#    plt.plot(TimeStamp, GPSSpeed)
+    plt.plot(TimeStamp, Yaw, TimeStamp, Roll, TimeStamp, Pitch)
+    plt.show()
