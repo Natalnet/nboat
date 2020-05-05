ec435f5d6955bbd2eed73b37b936989c7332c28b
davihdsantos <davihenriqueds@gmail.com>
2020-05-05 20:23:03 -0300

    Update read_pot.ino
    qwdqd

--- Arduino/slave_vela_e_leme/read_pot.ino
+++ Arduino/slave_vela_e_leme/read_pot.ino
@@ -1,7 +1,7 @@
 int ler_angulo_atual_r(){
   int pot_r = analogRead(pinoPot_r);
 
-  //Serial.println(pot_r);
+  Serial.println(pot_r);
 
   //Serial.println(map(pot_r, pot_min_r, pot_max_r, -90, 90));
   
